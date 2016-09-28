7b209bec4cea73b4f749a52936007342169849b1
Marta Kobylinska <marta@onegini.com>
2016-09-28 15:14:09 +0200

    initial commit

---
+++ src/main/java/com/onegini/examples/ExampleRequestMapperApplication.java
@@ -0,0 +1,12 @@
+package com.onegini.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleRequestMapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleRequestMapperApplication.class, args);
+	}
+}
